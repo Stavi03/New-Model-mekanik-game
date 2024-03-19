d9a788c115fba76cf9ef913dc968bb7ab7e50356
Stavi03 <ramadanfakhri@gmail.com>
2024-03-19 11:32:47 +0700

    update jalan

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 500;
-    canvas.height = 500;
+    canvas.width = 1000;
+    canvas.height = 1000;
 
     class Game{
         constructor(width, height){
