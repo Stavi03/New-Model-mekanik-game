81752334274b1220ff96761e5a4bb55fa1cdb46d
boshtonnn <22082010243@student.upnjatim.ac.id>
2024-03-20 08:50:48 +0700

    UPDATE ENEMY

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { Player } from './player.js';
 import { InputHandler } from './input.js';
-// import { Background } from './background.js';
+import { Background } from './background.js';
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from './enemis.js';
 
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
@@ -12,24 +13,52 @@ window.addEventListener('load', function(){
         constructor(width, height){
             this.width = width;
             this.height = height;
-            this.speed = 3;
-            // this.background = new Background(this);
+            this.groundMargin = 80;
+            this.speed = 0;
+            this.maxSpeed = 3;
+            this.background = new Background(this);
             this.player = new Player(this);
             this.Input = new InputHandler();
+            this.enemies = [];
+            this.enemyTimer = 0;
+            this,enemyInterval - 1000;
+
         }
-        update(){
-            // this.background.update();
+        update(deltaTime){
+            this.background.update();
             this.player.update(this.Input.keys);
+            // handleEnemies
+            if (this.enemyTimer > this.enemyInterval){
+                this.addEnemy();
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime;
+            }
+            this.enemies.forEach(enemy => {
+                enemy.update(deltaTime);
+                if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
+            });
         }
         draw(context){
-            // this.background.draw(context);
-             this.player.draw(context);
+            this.background.draw(context);
+            this.player.draw(context);
+            this,this.enemies.forEach(enemy => {
+                enemy.draw(context);
+            });
+        }
+        addEnemy(){
+            if (this.speed > 0 && Math.random() < 0.5 ) this.enemies.push(new GroundEnemy(this));
+            else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+
+            this.enemies.push(new FlyingEnemy(this));
+            console.log(this.enemies);
         }
     }
 
     const game = new Game(canvas.width, canvas.height);
     console.log(game);
-    
+    let lastTime = 0;
+
     function animate(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         game.update();
