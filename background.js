e98648b4f8bbdd44546ec53372bd43a055ed9d9d
Stavi03 <ramadanfakhri@gmail.com>
2024-03-21 08:02:59 +0700

    code background

--- background.js
+++ background.js
@@ -1,55 +1,55 @@
-// class layer {
-//     constructor(game, width, height, speedModifier, image){
-//         this.game = game;
-//         this.width = width;
-//         this.height = height;
-//         this.speedModifier = speedModifier;
-//         this.image = image;
-//         this.x = 0;
-//         this.y = 0;
-//     }
-//     update(){
-//         if (this.x < -this.width) this.x = 0;
-//         else this.x -= this.game.speed * this.speedModifier;
+class layer {
+    constructor(game, width, height, speedModifier, image){
+        this.game = game;
+        this.width = width;
+        this.height = height;
+        this.speedModifier = speedModifier;
+        this.image = image;
+        this.x = 0;
+        this.y = 0;
+    }
+    update(){
+        if (this.x < -this.width) this.x = 0;
+        else this.x -= this.game.speed * this.speedModifier;
 
-//     }
-//     draw(context){
-//         context.drawImage(this.image, this.x, this.y, this.widht, this.height);
+    }
+    draw(context){
+        context.drawImage(this.image, this.x, this.y, this.widht, this.height);
 
-//     }
-// }
+    }
+}
 
-// export class Background {
-//     constructor(game){
-//         this.game = game;
-//         this.widht = 1667;
-//         this.height = 500;
-//         this.layer1image = document.getElementById('layer1');
-//         this.layer2image = document.getElementById('layer2');
-//         this.layer3image = document.getElementById('layer3');
-//         this.layer4image = document.getElementById('layer4');
-//         this.layer5image = document.getElementById('layer5');
-//         this.layer1 = new layer(this.game, this.widht, this.height, 1, this.layer1image)
-//         this.layer2 = new layer(this.game, this.widht, this.height, 1, this.layer2image)
-//         this.layer3 = new layer(this.game, this.widht, this.height, 1, this.layer3image)
-//         this.layer4 = new layer(this.game, this.widht, this.height, 1, this.layer4image)
-//         this.layer5 = new layer(this.game, this.widht, this.height, 1, this.layer5image)
-//         this.BackgroundLayers = [this.layer1, this.layer2, this.layer3, this.layer4, this.layer5,]
-//     }
-//     update(){
-//         this.BackgroundLayers.forEach(layer => {
-//            layer.update();
+export class Background {
+    constructor(game){
+        this.game = game;
+        this.widht = 1667;
+        this.height = 500;
+        this.layer1image = document.getElementById('layer1');
+        this.layer2image = document.getElementById('layer2');
+        this.layer3image = document.getElementById('layer3');
+        this.layer4image = document.getElementById('layer4');
+        this.layer5image = document.getElementById('layer5');
+        this.layer1 = new layer(this.game, this.widht, this.height, 1, this.layer1image)
+        this.layer2 = new layer(this.game, this.widht, this.height, 1, this.layer2image)
+        this.layer3 = new layer(this.game, this.widht, this.height, 1, this.layer3image)
+        this.layer4 = new layer(this.game, this.widht, this.height, 1, this.layer4image)
+        this.layer5 = new layer(this.game, this.widht, this.height, 1, this.layer5image)
+        this.BackgroundLayers = [this.layer1, this.layer2, this.layer3, this.layer4, this.layer5,]
+    }
+    update(){
+        this.BackgroundLayers.forEach(layer => {
+           layer.update();
 
 
         
-//         })
-//     }
-//     draw(context){
-//         this.BackgroundLayers.forEach(layer => {
-//            layer.draw(context);
+        })
+    }
+    draw(context){
+        this.BackgroundLayers.forEach(layer => {
+           layer.draw(context);
             
 
         
-//         })
-//     }    
-// }
+        })
+    }    
+}
