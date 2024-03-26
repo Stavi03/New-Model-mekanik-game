8132e59d431ea26020ca96a8a5cca7d9263f0b08
Stavi03 <ramadanfakhri@gmail.com>
2024-03-26 14:39:21 +0700

    background fix

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-class layer {
+class Layer {
     constructor(game, width, height, speedModifier, image){
         this.game = game;
         this.width = width;
@@ -11,45 +11,37 @@ class layer {
     update(){
         if (this.x < -this.width) this.x = 0;
         else this.x -= this.game.speed * this.speedModifier;
-
     }
     draw(context){
-        context.drawImage(this.image, this.x, this.y, this.widht, this.height);
-
+        context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
     }
 }
-
 export class Background {
     constructor(game){
         this.game = game;
-        this.widht = 1667;
+        this.width = 1667;
         this.height = 500;
         this.layer1image = document.getElementById('layer1');
         this.layer2image = document.getElementById('layer2');
         this.layer3image = document.getElementById('layer3');
         this.layer4image = document.getElementById('layer4');
         this.layer5image = document.getElementById('layer5');
-        this.layer1 = new layer(this.game, this.widht, this.height, 1, this.layer1image)
-        this.layer2 = new layer(this.game, this.widht, this.height, 1, this.layer2image)
-        this.layer3 = new layer(this.game, this.widht, this.height, 1, this.layer3image)
-        this.layer4 = new layer(this.game, this.widht, this.height, 1, this.layer4image)
-        this.layer5 = new layer(this.game, this.widht, this.height, 1, this.layer5image)
-        this.BackgroundLayers = [this.layer1, this.layer2, this.layer3, this.layer4, this.layer5,]
+        this.layer1 = new Layer(this.game, this.width, this.height, 0, this.layer1image);
+        this.layer2 = new Layer(this.game, this.width, this.height, 0.2, this.layer2image);
+        this.layer3 = new Layer(this.game, this.width, this.height, 0.4, this.layer3image);
+        this.layer4 = new Layer(this.game, this.width, this.height, 0.8, this.layer4image);
+        this.layer5 = new Layer(this.game, this.width, this.height, 1, this.layer5image);
+        this.backgroundLayers = [this.layer1, this.layer2, this.layer3, this.layer4, this.layer5];
     }
     update(){
-        this.BackgroundLayers.forEach(layer => {
-           layer.update();
-
-
-        
+        this.backgroundLayers.forEach(layer =>{
+            layer.update();
         })
     }
     draw(context){
-        this.BackgroundLayers.forEach(layer => {
-           layer.draw(context);
-            
-
-        
+        this.backgroundLayers.forEach(layer =>{
+            layer.draw(context);
         })
-    }    
+    }
 }
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { Background } from './background.js';
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 1000;
+    canvas.width = 500;
     canvas.height = 500;
 
     class Game{
